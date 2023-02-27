9ef4c5dd4c942c6ef28a310e1aef221009d8ea69
cyril.toneatto@gmail.com <cyril.toneatto@gmail.com>
2023-02-27 15:23:32 +0000

    Model Commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # astrafy-covid
+Astrafy Covid Model:
+
+This model is based on multiple sources:
+- The New York Times US Coronavirus Database: https://github.com/nytimes/covid-19-data
+- US Vaccinations: https://github.com/owid/covid-19-data/tree/master/public/data/vaccinations/
+- US Population: https://worldpopulationreview.com/states
+- States Boundaries: Bigquery Public Data
+
+With this model we try to understand the global state of covid19, correlations between deaths, cases and vaccinations and to identify some patterns.
+
+This model can be improved with more reliable et detail data. 
+For instance, the population sources could be better with a daily or monthly level of detail, moreover there are some missing data (e.g. 2021 population)
+
+The Looker Studio dashboard: https://lookerstudio.google.com/reporting/9bfbc430-c68d-404e-9b95-78c5ca570e51/page/BChGD
---
+++ models/fact_us_covid.sql
@@ -0,0 +1,41 @@
+{{
+  config(
+    materialized='view',
+    schema='ds_nyt_covid'
+  )
+}}
+
+WITH tmp_us_states AS (
+    SELECT
+        *
+    ,LAG(confirmed_cases) OVER (PARTITION BY state_fips_code ORDER BY state_fips_code, date ASC) AS lag_confirmed_cases
+    ,LAG(deaths) OVER (PARTITION BY state_fips_code ORDER BY state_fips_code, date ASC) AS lag_deaths
+    FROM {{ source('covid19_nyt', 'us_states') }}
+)
+
+SELECT
+  -- Primary key
+  uss.date 
+  ,uss.state_fips_code
+  -- Global infos --
+  ,uss.state_name
+  ,uss.confirmed_cases
+  ,uss.deaths
+  ,IF(uss.confirmed_cases >= uss.lag_confirmed_cases, uss.confirmed_cases - uss.lag_confirmed_cases, 0)  AS new_confirmed_cases
+  ,IF(uss.deaths >= uss.lag_deaths, uss.deaths - uss.lag_deaths, 0)  AS new_deaths
+  -- Vaccination infos
+  ,sv.daily_vaccinations
+  -- Population infos
+  ,CASE EXTRACT(YEAR FROM uss.date)
+    WHEN 2020 THEN sp.pop2020
+    WHEN 2021 THEN sp.pop2020
+    WHEN 2022 THEN sp.pop2022
+    WHEN 2023 THEN sp.pop2023
+  END AS year_population
+  -- Geo infos
+  ,gs.geo_id 
+  ,ST_SIMPLIFY(gs.state_geom, 10000) AS state_geom
+FROM tmp_us_states uss
+LEFT JOIN {{ source('geo_us_boundaries', 'states') }} gs ON gs.state_fips_code = uss.state_fips_code 
+LEFT JOIN {{ source('ds_public_data', 'us_states_vaccinations') }} sv ON sv.date = uss.date AND sv.location = uss.state_name
+LEFT JOIN {{ source('ds_public_data', 'us_states_population') }} sp ON sp.fips = SAFE_CAST(uss.state_fips_code AS INT64) 
